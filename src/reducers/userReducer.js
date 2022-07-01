fb36b064f771353d7313ccb4c1c1801ae27a2e6d
ANURAG <1928081@kiit.ac.in>
2022-07-02 01:37:49 +0530

    saga

---
+++ src/reducers/userReducer.js
@@ -0,0 +1,9 @@
+const initialState = undefined;
+
+export default function reducer(state = initialState, action) {
+    switch (action.type) {
+        //case 1
+
+        default: return state;
+    }
+}
